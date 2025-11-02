f1d1163959ab252e0a3a0f3d42c6779ebbe9fed4
sanavi-rahane <sanavirahane2005@gmail.com>
2025-11-02 22:28:26 +0530

    modified 17_1 file

--- Assignments/Assignment_17/program17_2.java
+++ Assignments/Assignment_17/program17_2.java
@@ -3,7 +3,18 @@ class Logic
 {
     void checkPalidrome(int num)
     {
-        
+        int iNo = num;
+        int reverse = 0;
+        int digit = 0;
+        while (num!=0) {
+            digit = num % 10;
+            reverse =reverse*10+digit;
+            num = num /10;
+        }
+        if(iNo == reverse)
+        System.out.println("Palidrome");
+        else
+        System.out.println("Not palidrome");
     }
 }
 public class program17_2 {
